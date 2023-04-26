6e9c9790f800f478e33a41236f6f7c316a478017
paula <paula_ribe93@hotmail.com>
2023-04-26 18:32:34 +0200

    feat: Created detail slice page

---
+++ L-agulla-de-cosir/src/app/store.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import thunk from 'redux-thunk';
+import detailSlice from '../pages/Slices/detailSlice';
+import userSlice from '../pages/Slices/userSlice';
+
+const reducers = combineReducers({
+    user: userSlice,
+    detail: detailSlice
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk]
+});
